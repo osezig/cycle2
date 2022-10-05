edce49a8a5dabff6a4acc9c90e56e0b554ca38ce
sergey_iagfarov <dedaz27@mail.ru>
2022-10-05 21:46:58 +0500

    задание 5

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 1904; i < 2096; i = i + 4) {
+
+            System.out.println("Год является високосным " + i);
+        }
+    }
+}
